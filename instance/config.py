61c7eaff70334c149b977883751d0f0a476fef29
Szymon Miks <szymonm@miroart.pl>
2018-03-23 10:31:37 +0100

    change instance/config file

--- instance/config.py
+++ instance/config.py
@@ -1,4 +1,5 @@
+import os
 from boto.s3.connection import S3Connection
 
-SECRET_KEY = ''
-SQLALCHEMY_DATABASE_URI = 'mysql://root@localhost/url_shortener'
+SECRET_KEY = os.environ['SECRET_KEY']
+SQLALCHEMY_DATABASE_URI = os.environ['SQLALCHEMY_DATABASE_URI']
