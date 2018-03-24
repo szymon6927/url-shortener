06e396591c65f37dab5d04cf314e844583a20385
Szymon Miks <miks.szymon@gmail.com>
2018-03-24 11:19:27 +0100

    add templete for redirect view, and read in redirect.js ga_client_id and send to backend

---
+++ app/static/js/redirect.js
@@ -0,0 +1,36 @@
+function check_ga() {
+    if (typeof ga === 'function') {
+        console.log('Loaded :' + ga);
+        runAjax();
+    }
+    else {
+        console.log('Not loaded');
+        setTimeout(check_ga, 500);
+    }
+}
+
+function runAjax() {
+    var hash = $("#hash").val();
+    var client_id = ga.getAll()[0].get('clientId');
+    console.log("Starting ajax");
+
+    $.ajax({
+        type: "POST",
+        url: "/redirect",
+        data: {
+            hash: hash,
+            ga_client_id: client_id
+        },
+        success: function (data) {
+            data = JSON.parse(data);
+            console.log(data.info);
+        },
+        error: function (data) {
+            console.log(data);
+        }
+    });
+}
+
+$(document).ready(function () {
+    check_ga();
+});
