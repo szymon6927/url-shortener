9aae2f63cfa83fe2beec7018544738b230edbc2f
Szymon Miks <miks.szymon@gmail.com>
2018-03-27 23:04:51 +0200

    add new User model for registration and login'

--- app/models.py
+++ app/models.py
@@ -1,6 +1,50 @@
 import datetime
-from app import db
+from app import db, login_manager
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
+
+class Users(db.Model, UserMixin):
+    """
+    Create a Users table
+    """
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    email = db.Column(db.String(60), unique=True)
+    username = db.Column(db.String(60), unique=True)
+    first_name = db.Column(db.String(60))
+    last_name = db.Column(db.String(60))
+    password_hash = db.Column(db.String(128))
+    created_at = db.Column(db.DateTime, default=datetime.datetime.now)
+    is_admin = db.Column(db.Boolean, default=False)
+
+    @property
+    def password(self):
+        """
+        Prevent pasword from being accessed
+        """
+        raise AttributeError("Password is not readable attribute")
+
+    @password.setter
+    def password(self, password):
+        """
+        Set password to a hashed password
+        """
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        """
+        Check if hashed password matches actual password
+        """
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return "<Users: {}>".format(self.email)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return Users.query.get(int(user_id))
 
 class Links(db.Model):
     """
