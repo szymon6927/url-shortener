9ec5731ca5f25f7ecdfbb74e48f33172ba1e4f00
Szymon Miks <miks.szymon@gmail.com>
2018-03-30 00:26:43 +0200

    add config for test

--- config.py
+++ config.py
@@ -2,16 +2,17 @@ class Config(object):
     """
     Common configurations
     """
-
     # Put any configurations here that are common across all environments
+    DEBUG = True
+
 
 class DevelopmentConfig(Config):
     """
     Development configurations
     """
-    DEBUG = True
     SQLALCHEMY_ECHO = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
 class ProductionConfig(Config):
     """
@@ -20,7 +21,17 @@ class ProductionConfig(Config):
 
     DEBUG = False
 
+
+class TestConfig(Config):
+    """
+    Testing configurations
+    """
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    TESTING = True
+
+
 app_config = {
     'development': DevelopmentConfig,
-    'production': ProductionConfig
+    'production': ProductionConfig,
+    'testing': TestConfig
 }
